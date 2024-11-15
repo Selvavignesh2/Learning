8b1e6cc4709a12d74896a90a7a4d5176289101f1
Ubuntu <ubuntu@ip-172-31-1-188.ap-south-1.compute.internal>
2024-11-15 08:22:00 +0000

    new modified

--- groceryapp/app.py
+++ groceryapp/app.py
@@ -18,5 +18,5 @@ def home():
     return render_template('index.html', groceries=groceries)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',port=9090)
 
