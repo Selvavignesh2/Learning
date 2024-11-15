5070c63f63bf882d9b574f287bd3ced54b9a5e23
Ubuntu <ubuntu@ip-172-31-1-188.ap-south-1.compute.internal>
2024-11-15 06:42:56 +0000

    added new app

---
+++ groceryapp/app.py
@@ -0,0 +1,22 @@
+
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Dummy data for groceries
+groceries = [
+    {'name': 'Apple', 'price': 3.5},
+    {'name': 'Banana', 'price': 1.2},
+    {'name': 'Carrot', 'price': 0.8},
+    {'name': 'Milk', 'price': 1.5}
+]
+
+@app.route('/')
+def home():
+    return render_template('index.html', groceries=groceries)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
